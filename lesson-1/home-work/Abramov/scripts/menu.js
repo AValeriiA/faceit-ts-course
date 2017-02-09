1eaf996d7bba961bcb3bb99288a876d03e62c363
Valerii Abramov <valeriy.abramov@faceit.com.ua>
2017-02-09 16:43:23 +0200

    first commit

---
+++ lesson-1/home-work/Abramov/scripts/menu.js
@@ -0,0 +1,66 @@
+var menuList = [
+    {
+        title: 'Животные', items: [
+            {
+                title: 'Млекопитающие', items: [
+                    { title: 'Коровы' },
+                    { title: 'Ослы' },
+                    { title: 'Собаки' },
+                    { title: 'Тигры' }
+                ]
+            },
+            {
+                title: 'Другие', items: [
+                    { title: 'Змеи' },
+                    { title: 'Птицы' },
+                    {
+                        title: 'Другие', items: [
+                            { title: 'Змеи' },
+                            { title: 'Птицы' },
+                            { title: 'Ящерицы' },
+                        ],
+                    },
+                ],
+            },
+        ]
+    },
+    {
+        title: 'Рыбы', items: [
+            {
+                title: 'Аквариумные', items: [
+                    { title: 'Гуппи' },
+                    { title: 'Скалярии' }
+                ]
+            },
+            {
+                title: 'Форель', items: [
+                    { title: 'Морская форель' }
+                ]
+            },
+        ]
+    }
+];
+function generateMenu(list) {
+    var str = "<ul>";
+    for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+        var menu = list_1[_i];
+        str += "<li><a class=\"title\">" + menu.title + "</a>";
+        if (menu.items) {
+            str += generateMenu(menu.items);
+        }
+        str += "</li>";
+    }
+    str += "</ul>";
+    return str;
+}
+var navMenuList = document.querySelector('.menu');
+navMenuList.innerHTML = generateMenu(menuList);
+navMenuList.onclick = function (e) {
+    var el = e.target;
+    var classList = el.classList;
+    if (classList.contains('title')) {
+        var parentLi = el.parentNode;
+        parentLi.classList.toggle('menu-open');
+    }
+};
+//# sourceMappingURL=menu.js.map
---
+++ lesson-1/home-work/Abramov/scripts/menu.ts
@@ -0,0 +1,73 @@
+type menuList = {title: string, items?: menuList }[];
+let menuList: menuList = [
+  {
+    title: 'Животные', items: [
+    {
+      title: 'Млекопитающие', items: [
+      {title: 'Коровы'},
+      {title: 'Ослы'},
+      {title: 'Собаки'},
+      {title: 'Тигры'}
+    ]
+    },
+    {
+      title: 'Другие', items: [
+      {title: 'Змеи'},
+      {title: 'Птицы'},
+      {
+        title: 'Другие', items: [
+        {title: 'Змеи'},
+        {title: 'Птицы'},
+        {title: 'Ящерицы'},
+      ],
+      },
+    ],
+    },
+  ]
+  },
+  {
+    title: 'Рыбы', items: [
+    {
+      title: 'Аквариумные', items: [
+      {title: 'Гуппи'},
+      {title: 'Скалярии'}
+    ]
+    },
+    {
+      title: 'Форель', items: [
+      {title: 'Морская форель'}
+    ]
+    },
+  ]
+  }
+];
+
+function generateMenu(list: menuList):string {
+
+  let str: string = `<ul>`;
+
+  for(let menu of list) {
+    str += `<li><a class="title">${menu.title}</a>`;
+
+    if(menu.items) {
+      str += generateMenu(menu.items);
+    }
+    str += `</li>`;
+  }
+  str += `</ul>`;
+
+  return str;
+}
+
+let navMenuList: HTMLElement = document.querySelector('.menu') as HTMLElement;
+
+navMenuList.innerHTML = generateMenu(menuList);
+
+navMenuList.onclick = (e: MouseEvent) => {
+  let el = e.target as HTMLElement;
+  let classList = el.classList;
+  if(classList.contains('title')) {
+    let parentLi = el.parentNode as HTMLElement;
+    parentLi.classList.toggle('menu-open');
+  }
+};
